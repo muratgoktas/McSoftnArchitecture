faaad8e9503b192eb49713d094f3feaa6a1afb55
Murat GÖKTAŞ <mcs.goktas@gmail.com>
2024-03-13 19:11:07 +0300

    oses-05 Appliation, Program changed.

---
+++ src/demoProjects/AKUTPro/Application/Features/Products/Profiles/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using Application.Features.Products.Commands.CreateProduct;
+using Application.Features.Products.Dtos;
+using AutoMapper;
+using Domain.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Products.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, CreatedProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductCommand>().ReverseMap();
+            
+        }
+    }
+}
